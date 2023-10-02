4a90e9ecca2197efed05dc881a206c678d801863
Rezwan66 <shaikhrezwan66@gmail.com>
2023-10-03 04:11:32 +0600

    created shared components like header, nav, left and right aside

---
+++ src/components/Shared/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h2 className="text-3xl">This is the header</h2>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import Header from '../components/Shared/Header';
+import Navbar from '../components/Shared/Navbar';
+
 const Home = () => {
   return (
     <div>
+      <Header></Header>
+      <Navbar></Navbar>
       <h2 className="text-3xl">This is the homepage</h2>
     </div>
   );
